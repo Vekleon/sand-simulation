351fe02e3734ce11c3b2f284b942fda6c244d176
Vekleon <purplemenace297@gmail.com>
2021-12-18 19:23:49 -0500

    added more to stress calculations.

--- include/frictional_stress.h
+++ include/frictional_stress.h
@@ -2,6 +2,7 @@
 #include <EigenTypes.h>
 #include <newtons_method.h>
 
-void frictional_stress(Eigen::MatrixXd frictions, Eigen::VectorXd q, Eigen::VectorXd qdot,
+void frictional_stress(Eigen::MatrixXd& stress, double& mean_stress, double& shear_stress, 
+                        Eigen::VectorXd q, Eigen::VectorXd qdot,
                         Eigen::Vector3d p0, Eigen::TensorP& P, Eigen::TensorXV& xv, 
                         Eigen::TensorYV& yv, Eigen::TensorZV& zv, const double dg);
--- src/frictional_stress.cpp
+++ src/frictional_stress.cpp
@@ -1,10 +1,11 @@
 #include <frictional_stress.h>
 #include <cmath>
 
-void frictional_stress(Eigen::MatrixXd frictions, Eigen::VectorXd& q, Eigen::VectorXd& qdot,
-                        Eigen::Vector3d& p0, Eigen::TensorP& P, Eigen::TensorXV& xv, 
+void frictional_stress(Eigen::MatrixXd& stress, double& mean_stress, double& shear_stress, 
+                        Eigen::VectorXd q, Eigen::VectorXd qdot,
+                        Eigen::Vector3d p0, Eigen::TensorP& P, Eigen::TensorXV& xv, 
                         Eigen::TensorYV& yv, Eigen::TensorZV& zv, const double dg) {
-    
+    stress.resize(q.size() / 3, q.size() / 3);
     Eigen::MatrixXd GRID_OFFSETS(4, 3);
 	GRID_OFFSETS <<
 		0.5, 0.5, 0.0,
@@ -21,4 +22,14 @@ void frictional_stress(Eigen::MatrixXd frictions, Eigen::VectorXd& q, Eigen::Vec
         Eigen::Vector3i cell_index = ((1. / dg) * (particle_pos - p0 + GRID_OFFSETS.row(3).transpose()));
         
     }
+
+    mean_stress = stress.trace() / 3;
+    Eigen::MatrixXd delta;
+    delta.resize(q.size() / 3, q.size() / 3);
+    for(int row = 0; row < delta.rows(); row++){
+        for(int col = 0; col < delta.cols(); col++){
+            delta(row, col) = 0.1 * mean_stress;
+        }
+    }
+    shear_stress = (stress - delta).norm() / (std::sqrt(2));
 }
