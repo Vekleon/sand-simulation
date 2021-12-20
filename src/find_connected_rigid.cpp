23fbb7341c9608211bf41ed85007207ff3749b51
Vekleon <purplemenace297@gmail.com>
2021-12-20 15:56:39 -0500

    Added code to connect groups of rigid grids. hopefully it's right.

--- src/find_connected_rigid.cpp
+++ src/find_connected_rigid.cpp
@@ -1,24 +1,48 @@
 #include <find_connected_rigid.h>
 
-void traverse(std::set<int>& new_connection, Eigen::TensorRF& rigid_mapping, int x, int y, int z) {
+bool out_of_bounds(int x, int y, int z) {
+    return x >= TENSOR_P_X || y >= TENSOR_P_Y || z >= TENSOR_P_Z;
+}
 
+void traverse(std::set<int>& explored, std::set<int>& new_connection, Eigen::TensorRF& rigid_mapping, int x, int y, int z) {
+    int target = get_cell_idx(x, y, z, TENSOR_P_X, TENSOR_P_Y, TENSOR_P_Z);
+    if(out_of_bounds(x, y, z) || !rigid_mapping[x][y][z] || explored.find(target) != explored.end()) {
+        explored.insert(target);
+        return;
+    }
+
+    new_connection.insert(target);
+    explored.insert(target);
+    traverse(explored, new_connection, rigid_mapping, x + 1, y , z);
+    traverse(explored, new_connection, rigid_mapping, x - 1, y , z);
+    traverse(explored, new_connection, rigid_mapping, x, y + 1, z);
+    traverse(explored, new_connection, rigid_mapping, x, y - 1, z);
+    traverse(explored, new_connection, rigid_mapping, x, y, z + 1);
+    traverse(explored, new_connection, rigid_mapping, x, y, z - 1);
 }
 
 void find_connected_rigid(std::vector<std::set<int>>& connections, Eigen::TensorRF& rigid_mapping) {
-    std::set<int> added;
+    std::set<int> explored;
     int target;
 
     for(int z = 0; z < TENSOR_P_Z; z++) {
         for(int y = 0; y < TENSOR_P_Y; y++) {
             for(int x = 0; x < TENSOR_P_X; x++) {
                 target = get_cell_idx(x, y, z, TENSOR_P_X, TENSOR_P_Y, TENSOR_P_Z);
-                if(added.find(target) != added.end() || !rigid_mapping[x][y][z]) continue;
+                if(!rigid_mapping[x][y][z] || explored.find(target) != explored.end()) {
+                    explored.insert(target);
+                    continue;
+                }
                 std::set<int> new_connection {target};
-                traverse(new_connection, rigid_mapping, x + 1, y , z);
-                traverse(new_connection, rigid_mapping, x, y + 1, z);
-                traverse(new_connection, rigid_mapping, x, y, z + 1);
+                traverse(explored, new_connection, rigid_mapping, x + 1, y , z);
+                traverse(explored, new_connection, rigid_mapping, x - 1, y , z);
+                traverse(explored, new_connection, rigid_mapping, x, y + 1, z);
+                traverse(explored, new_connection, rigid_mapping, x, y - 1, z);
+                traverse(explored, new_connection, rigid_mapping, x, y, z + 1);
+                traverse(explored, new_connection, rigid_mapping, x, y, z - 1);
 
-                added.insert(new_connection.begin(), new_connection.end());
+                explored.insert(new_connection.begin(), new_connection.end());
+                connections.push_back(new_connection);
             }
         }
     }
